93e0c89e0eb324a8c33c2bf47667e45d097307c5
KolomiAnna <115336722+KolomiAnna@users.noreply.github.com>
2023-04-10 08:34:04 +0300

    485

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const btnLoadMoreEl = document.querySelector(".load-more");
 
 let page = 1;
 let per_page = 40;
+let q; 
 
 btnLoadMoreEl.classList.add("is-hidden");
 alertPopup.classList.add("is-hidden");
@@ -24,11 +25,15 @@ btnLoadMoreEl.addEventListener("click", () => {
     }
 
     page += 1;
+   
+  
+
     getPic(searchQuery).then(data => {
         createGallery(data);
-        console.log(data.hits.length);
+        console.log(data.totalHits / 40);
+        console.log(page);
 
-        if (data.hits.length < per_page) {
+        if (data.totalHits / 40 < page) {
             btnLoadMoreEl.classList.add("is-hidden");
             alertPopup.classList.remove("is-hidden");
         }
@@ -56,7 +61,9 @@ formEl.addEventListener("submit", handlerSubmit);
 async function handlerSubmit(event) {
     event.preventDefault();
 
-    const searchQuery = inputEl.value.trim();
+    const searchQuery = event.target.elements["searchQuery"].value.trim();
+
+    q = searchQuery;
 
     if (!searchQuery) {
         return;
@@ -64,13 +71,25 @@ async function handlerSubmit(event) {
     console.log(`getPic = ${searchQuery}`);
 
     getPic(searchQuery).then(data => {
-         createGallery(data);
+        createGallery(data);
+        
 
-        if (!data.hits.length) {
+        console.log(data.totalHits / 40);
+        console.log(page);
+
+        if (!data.hits.length)
+        {
             btnLoadMoreEl.classList.add("is-hidden");
             throw new Error(Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."));
+        } 
+    
+        if (data.totalHits / 40 < page) {
+            alertPopup.classList.remove("is-hidden");
         }
-        btnLoadMoreEl.classList.remove("is-hidden");
+
+        if (data.totalHits / 40 > page) {
+            btnLoadMoreEl.classList.remove("is-hidden");
+        }
     })
 }
 
@@ -113,93 +132,3 @@ function createGallery(name) {
 
 
 
-
-
-// let page = 1;
-// let per_page = 40;
-
-// btnLoadMoreEl.classList.add("is-hidden");
-// alertPopup.classList.add("is-hidden");
-
-
-// btnLoadMoreEl.addEventListener("click", () => {
-//     const searchQuery = inputEl.value.trim();
-
-//     if (!searchQuery) {
-//         return;
-//     }
-
-//     page += 1;
-//     getPic(searchQuery).then(data => {
-//         createGallery(data);
-//         console.log(data.hits.length);
-
-//         if (data.hits.length < per_page) {
-//             btnLoadMoreEl.classList.add("is-hidden");
-//             alertPopup.classList.remove("is-hidden");
-//         }
-//     })
-//         .catch((error) => console.log(error));
-// });
-
-
-
-// function getPic(nameImgs) {
-
-//     return fetch(`https://pixabay.com/api/?key=35143427-54cddf882f78b9182e7e3e5c2&q=${nameImgs}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`).then(
-//         (response) => {
-//             if (!response.ok) {
-//                 throw new Error(response.status);
-//             }
-//             return response.json();
-//         }
-//     )
-// }
-
-
-// formEl.addEventListener("submit", handlerSubmit);
-
-// function handlerSubmit(event) {
-//     event.preventDefault();
-
-//     const searchQuery = inputEl.value.trim();
-
-//     if (!searchQuery) {
-//         return;
-//     }
-//     console.log(`getPic = ${searchQuery}`);
-
-//     getPic(searchQuery).then(data => {
-//         createGallery(data);
-
-//         console.log(data.hits.length);
-//         if (!data.hits.length) {
-//             btnLoadMoreEl.classList.add("is-hidden");
-//             throw new Error(Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again."));
-//         }
-//         btnLoadMoreEl.classList.remove("is-hidden");
-//     })
-// }
-
-
-// function createGallery(name) {
-//     const markupName = (name.hits).map(element =>
-//         `<div class="photo-card">
-//     <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" width="350px"/>
-//     <div class="info" style="display:flex; gap:15px; ">
-//         <p class="info-item">
-//             <b>Likes<br>${element.likes}</b>
-//         </p>
-//         <p class="info-item">
-//             <b>Views<br>${element.views}</b>
-//         </p>
-//         <p class="info-item">
-//             <b>Comments<br>${element.comments}</b>
-//         </p>
-//         <p class="info-item">
-//             <b>Downloads<br>${element.downloads}</b>
-//         </p>
-//     </div>
-// </div>`).join("");
-//     galleryEl.insertAdjacentHTML("beforeend", markupName);
-// };
